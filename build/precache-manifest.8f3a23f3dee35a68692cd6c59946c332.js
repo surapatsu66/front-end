437976b8ef84ff3e01bbfad4e35b1c9fb49574d5
surapatsu66 <surapat_su66@live.rmutl.ac.th>
2024-02-09 14:56:39 +0000

    Add build server

---
+++ build/precache-manifest.8f3a23f3dee35a68692cd6c59946c332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ced84ad74ac09fb24f796a458550891",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80e2feadc96181329bef",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "a7fa47f39273e77bb272",
+    "url": "/static/css/main.c685ad9d.chunk.css"
+  },
+  {
+    "revision": "80e2feadc96181329bef",
+    "url": "/static/js/2.9810f920.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.9810f920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7fa47f39273e77bb272",
+    "url": "/static/js/main.cac03b14.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  }
+]);
