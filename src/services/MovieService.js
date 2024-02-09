4969730f517282e15561489ca6b78d4d7e875bba
surapatsu66 <surapat_su66@live.rmutl.ac.th>
2024-02-09 12:50:39 +0700

    MovieSerEdit

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -2,7 +2,7 @@ export async function getAllMovies() {
 
     try{
         //const response = await fetch('/api/users');
-         const response = await fetch('http://api.se-rmutl.net/api/movie/all');
+         const response = await fetch('https://api.se-rmutl.net/api/movie/all');
         //const response = await fetch('/api/movie/all');
         return await response.json();
     }catch(error) {
@@ -13,7 +13,7 @@ export async function getAllMovies() {
 //http://localhost:3001/api/movie/insert
 //---- ยังไม่เสร็จ ----
 export async function createMovie(data) {
-    const response = await fetch(`http://api.se-rmutl.net/api/movie/insert`, {
+    const response = await fetch(`https://api.se-rmutl.net/api/movie/insert`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({...data})
